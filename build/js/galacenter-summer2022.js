2579e9e0892ebdbc0eacd7fb14ebdede40272787
Maxim Karachyov <karachyov.m@gmail.com>
2022-05-17 16:05:15 +0300

    populary-summer2022.scss

--- build/js/galacenter-summer2022.js
+++ build/js/galacenter-summer2022.js
@@ -1,3 +1,32 @@
+const swiperPopularySummer2022 = new Swiper('.swiper-populary-summer2022', {
+    slidesPerView: 5,
+    spaceBetween:50,
+    loop:false,
+    watchOverflow: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    observer: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween:30,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween:30,
+        },
+        900: {
+            slidesPerView: 4,
+            spaceBetween:40,
+        },
+        1024: {
+            slidesPerView: 5,
+        }
+    }
+
+
+});
+
 const swiperBrandSummer2022 = new Swiper('.swiper-brand-summer2022', {
     slidesPerView: 4,
     spaceBetween:67,

7d4a9c57d9c3d55909fe40a5b70e72a57b9b9dac
Maxim Karachyov <karachyov.m@gmail.com>
2022-05-17 15:14:32 +0300

    project
    # Conflicts:
    #	README.md

---
+++ src/js/galacenter-summer2022.js
@@ -0,0 +1,30 @@
+const swiperBrandSummer2022 = new Swiper('.swiper-brand-summer2022', {
+    slidesPerView: 4,
+    spaceBetween:67,
+    loop:false,
+    watchOverflow: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    observer: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween:30,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween:30,
+        },
+        900: {
+            slidesPerView: 3,
+            spaceBetween:40,
+        },
+        1024: {
+            slidesPerView: 4,
+        }
+    }
+
+
+});
+
+

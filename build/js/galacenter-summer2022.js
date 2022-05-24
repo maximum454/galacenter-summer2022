767f1336fe7501afe610313f9c352e0cd9610f32
Maxim Karachyov <karachyov.m@gmail.com>
2022-05-24 12:48:22 +0300

    brand fix

--- build/js/galacenter-summer2022.js
+++ build/js/galacenter-summer2022.js
@@ -44,33 +44,4 @@ const swiperPopularySummer2022 = new Swiper('.swiper-populary-summer2022', {
 
 });
 
-const swiperBrandSummer2022 = new Swiper('.swiper-brand-summer2022', {
-    slidesPerView: 4,
-    spaceBetween:67,
-    loop:false,
-    watchOverflow: true,
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
-    observer: true,
-    breakpoints: {
-        320: {
-            slidesPerView: 1,
-            spaceBetween:30,
-        },
-        768: {
-            slidesPerView: 2,
-            spaceBetween:30,
-        },
-        900: {
-            slidesPerView: 3,
-            spaceBetween:40,
-        },
-        1024: {
-            slidesPerView: 4,
-        }
-    }
-
-
-});
-
 

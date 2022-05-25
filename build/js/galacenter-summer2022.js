1515425d16dc19b732a7a09ad67da8e78a824ebd
Maxim Karachyov <karachyov.m@gmail.com>
2022-05-25 11:53:07 +0300

    fix

--- build/js/galacenter-summer2022.js
+++ build/js/galacenter-summer2022.js
@@ -1,3 +1,31 @@
+$(function () {
+    $('a[href^="#"]').on('click', function(event) {
+        // отменяем стандартное действие
+        event.preventDefault();
+
+        var sc = $(this).attr("href"),
+            dn = $(sc).offset().top;
+
+        $('html, body').animate({scrollTop: dn}, 1000);
+    });
+
+    $('.js-all-more').on('click', function (){
+        $('.all-block__item').removeClass('hidden');
+        $(this).addClass('d-none');
+    })
+
+    $('.js-slider-add').on('click', function (){
+        $('.slider-block').removeClass('hidden');
+        $(this).addClass('d-none');
+    })
+
+    $('.wrp').parent('.container').css({'max-width':'100%', 'padding':'0'});
+
+    $('.js-category-more').on('click', function (){
+        $('.supersubbota2022-category__body').removeClass('d-none')
+    })
+})
+
 const swiperMainSummer2022 = new Swiper('.swiper-main-summer2022', {
     slidesPerView: 1,
     loop:false,
